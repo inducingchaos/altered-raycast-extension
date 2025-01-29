d9c2b0856a83460d2d0af15ab112224c2b5fdbaa
Riley Barabash <admin@rileybarabash.com>
2025-01-29 08:01:50 -0700

    updated Clear All

--- src/domains/capture/actions/modify.tsx
+++ src/domains/capture/actions/modify.tsx
@@ -3,15 +3,20 @@
  */
 
 import { Action, ActionPanel, Icon } from "@raycast/api"
+import { useCaptureList } from "../components/provider"
 
 export function ModifyActions(): JSX.Element {
+    const { setDataStore } = useCaptureList()
+
     return (
         <ActionPanel.Section title="Modify">
             <Action
                 title="Clear All"
                 icon={Icon.Trash}
-                shortcut={{ modifiers: ["cmd"], key: "n" }}
-                onAction={() => console.log("Clear All")}
+                shortcut={{ modifiers: ["cmd", "shift"], key: "r" }}
+                onAction={() => {
+                    setDataStore(new Map())
+                }}
             />
         </ActionPanel.Section>
     )
