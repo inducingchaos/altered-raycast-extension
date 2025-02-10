f48e0c1f356368f9a406a3034209dc1d373f7086
Riley Barabash <admin@rileybarabash.com>
2025-02-09 20:16:06 -0700

    LFGGG

--- src/domains/altered/definitions/data/constraints/index.ts
+++ src/domains/altered/definitions/data/constraints/index.ts
@@ -21,100 +21,128 @@ export const dataConstraintUids = {
 export const dataConstraintIds = Object.keys(dataConstraintUids)
 export type DataConstraintID = keyof typeof dataConstraintUids
 
-export type DataConstraint<Options extends DataConstraintOptions = DataConstraintOptions, T extends Type = Type> = {
-    type: "constraint"
+export type DataConstraintOption<Schema extends Type = Type> = {
+    name: string
+    description: string
+} & (
+    | {
+          type: "group"
+          options: DataConstraintOptions<Schema>
+      }
+    | {
+          type: "value"
+          schema: Schema
+      }
+)
+
+export type DataConstraintOptions<Schema extends Type = Type> = Record<string, DataConstraintOption<Schema>>
+
+function createDataConstraintX<T extends DataConstraintOptions<U>, U extends Type>(options: T): T {
+    return options
+}
+
+type InferSchemaFromOptions<T extends DataConstraintOptions<Type>> = Expand<{
+    [Key in keyof T]: T[Key] extends { type: "value"; schema: Type }
+        ? T[Key]["schema"]["infer"]
+        : T[Key] extends { type: "group"; options: infer GroupOptions }
+          ? InferSchemaFromOptions<GroupOptions & DataConstraintOptions<Type>>
+          : never
+}>
+
+export type DataConstraint<T extends DataConstraintOptions> = {
+    // type: "constraint"
 
     id: DataConstraintID
-    name: string
-    description: string | ((options: InferSchemaFromOptions<Options>) => string)
+    // name: string
+    // description: string | ((options: InferSchemaFromOptions<T>) => string)
 
-    label: string | ((options: InferSchemaFromOptions<Options>) => string)
-    instructions?: string | ((options: InferSchemaFromOptions<Options>) => string)
-    error?: {
-        label?: string | ((options: InferSchemaFromOptions<Options>) => string)
-        description?: string | ((options: InferSchemaFromOptions<Options>) => string)
+    // label: string | ((options: InferSchemaFromOptions<T>) => string)
+    instructions?: string | ((options: InferSchemaFromOptions<T>) => string)
+    // error?: {
+    //     label?: string | ((options: InferSchemaFromOptions<T>) => string)
+    //     description?: string | ((options: InferSchemaFromOptions<T>) => string)
+    // }
+
+    // types: DataType["id"][]
+    // supersedes: DataConstraintID[]
+    options: T
+
+    // validate: (options: InferSchemaFromOptions<T>) => (value: string) => SafeParseReturnType<ZodSchema, unknown>
+}
+
+export function createDataConstraint<T extends DataConstraintOptions<U>, U extends Type>(
+    props: DataConstraint<T>
+): DataConstraint<T> {
+    return props
+}
+
+const testRange = createDataConstraint({
+    id: "range",
+    // name: "Range",
+    // description: "The range of the value.",
+    // label: options => {
+    //     return `Range: ${options.min}-${options.max}${options.step?.value ? `, Step: ${options.step.value}` : ""}`
+    // },
+    instructions: options =>
+        `The value must be between ${options.min} and ${options.max}${
+            options.step?.value ? `, with a step of ${options.step.value}` : ""
+        }.`,
+    // error: {
+    //     label: "Exceeds Range"
+    // },
+
+    // types: ["number"],
+    // supersedes: ["min-value", "max-value"],
+
+    // testOptions: type("number"),
+
+    options: {
+        min: {
+            type: "value",
+            name: "Minimum",
+            description: "The minimum value allowed.",
+            schema: type("number")
+        },
+        max: {
+            type: "value",
+            name: "Maximum",
+            description: "The maximum value allowed.",
+            schema: type("number")
+        },
+        step: {
+            type: "group",
+            name: "Step",
+            description: "Configure stepping behavior for the range.",
+            options: {
+                value: {
+                    type: "value",
+                    name: "Step Value",
+                    description: "The increment between allowed values.",
+                    schema: type("number | undefined")
+                },
+                offset: {
+                    type: "value",
+                    name: "Step Offset",
+                    description: "The starting point for step calculations.",
+                    schema: type("number | undefined")
+                }
+            }
+        }
     }
-
-    types: DataType["id"][]
-    supersedes: DataConstraintID[]
-    options: Options
-
-    testOptions: T
-
-    validate: (options: InferSchemaFromOptions<Options>) => (value: string) => SafeParseReturnType<ZodSchema, unknown>
-}
-
-// Helper function to create constraints with proper typing
-export function createDataConstraint<Options extends Record<string, DataConstraintOption>>(
-    props: Omit<DataConstraint<Options>, "type">
-) {
-    return { type: "constraint" as const, ...props }
-}
+})
 
 export const dataConstraints = {
-    range: createDataConstraint({
-        id: "range",
-        name: "Range",
-        description: "The range of the value.",
-        label: options => {
-            return `Range: ${options.min}-${options.max}${options.step?.value ? `, Step: ${options.step.value}` : ""}`
-        },
-        instructions: options =>
-            `The value must be between ${options.min} and ${options.max}${
-                options.step?.value ? `, with a step of ${options.step.value}` : ""
-            }.`,
-        error: {
-            label: "Exceeds Range"
-        },
-
-        types: ["number"],
-        supersedes: ["min-value", "max-value"],
-
-        testOptions: type("number"),
-
-        // options: {
-        //     min: {
-        //         type: "value",
-        //         name: "Minimum",
-        //         description: "The minimum value allowed.",
-        //         schema: type("number")
-        //     },
-        //     max: {
-        //         type: "value",
-        //         name: "Maximum",
-        //         description: "The maximum value allowed.",
-        //         schema: type("number")
-        //     },
-        //     step: {
-        //         type: "group",
-        //         name: "Step",
-        //         description: "Configure stepping behavior for the range.",
-        //         options: {
-        //             value: {
-        //                 type: "value",
-        //                 name: "Step Value",
-        //                 description: "The increment between allowed values.",
-        //                 schema: type("number | undefined")
-        //             },
-        //             offset: {
-        //                 type: "value",
-        //                 name: "Step Offset",
-        //                 description: "The starting point for step calculations.",
-        //                 schema: type("number | undefined")
-        //             }
-        //         }
-        //     }
-        // },
-        validate: options => value => {
-            const offset = options.step?.offset ?? 0
-            return z
-                .number({ coerce: true })
-                .min(options.min - offset)
-                .max(options.max - offset)
-                .step(options.step?.value ?? 1)
-                .safeParse(Number(value) - offset)
-        }
-    })
+    range: testRange
+    //     validate: options => value => {
+    //         const offset = options.step?.offset ?? 0
+    //         return z
+    //             .number({ coerce: true })
+    //             .min(options.min - offset)
+    //             .max(options.max - offset)
+    //             .step(options.step?.value ?? 1)
+    //             .safeParse(Number(value) - offset)
+    //     }
+    // })
 } as const
 
 // This stuff was for type inference for the config object before we made it recursive and partially literal instead of an entire ZodSchema.
@@ -138,68 +166,3 @@ export const dataConstraints = {
 //     id: ID
 //     options: DataConstraintConfigOptions<ID>
 // }
-
-// Recursive types as before
-export type DataConstraintOption<Schema extends Type = Type> = {
-    name: string
-    description: string
-} & (
-    | {
-          type: "group"
-          options: DataConstraintOptions<Schema>
-      }
-    | {
-          type: "value"
-          schema: Schema
-      }
-)
-
-export type DataConstraintOptions<Schema extends Type = Type> = Record<string, DataConstraintOption<Schema>>
-
-function createDataConstraint2<T extends DataConstraintOptions<U>, U extends Type>(options: T): T {
-    return options
-}
-
-const dataConstraint2 = createDataConstraint2({
-    min: {
-        type: "value",
-        name: "Minimum",
-        description: "The minimum value allowed.",
-        schema: type("number")
-    },
-    max: {
-        type: "value",
-        name: "Maximum",
-        description: "The maximum value allowed.",
-        schema: type("number")
-    },
-    step: {
-        type: "group",
-        name: "Step",
-        description: "Configure stepping behavior for the range.",
-        options: {
-            value: {
-                type: "value",
-                name: "Step Value",
-                description: "The increment between allowed values.",
-                schema: type("number | undefined")
-            },
-            offset: {
-                type: "value",
-                name: "Step Offset",
-                description: "The starting point for step calculations.",
-                schema: type("number | undefined")
-            }
-        }
-    }
-})
-
-type InferSchemaFromOptions<T extends DataConstraintOptions<Type>> = Expand<{
-    [Key in keyof T]: T[Key] extends { type: "value"; schema: Type }
-        ? T[Key]["schema"]["infer"]
-        : T[Key] extends { type: "group"; options: infer GroupOptions }
-          ? InferSchemaFromOptions<GroupOptions & DataConstraintOptions<Type>>
-          : never
-}>
-
-export type InferredSchema = InferSchemaFromOptions<typeof dataConstraint2>
