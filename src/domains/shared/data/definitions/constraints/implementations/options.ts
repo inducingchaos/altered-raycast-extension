443c9181a4161499c0ecda9d6e1263fc0881728f
Riley Barabash <admin@rileybarabash.com>
2025-02-14 13:51:08 -0700

    SA demo

--- src/domains/shared/data/definitions/constraints/implementations/options.ts
+++ src/domains/shared/data/definitions/constraints/implementations/options.ts
@@ -2,209 +2,82 @@
  *
  */
 
-import { Type, type } from "arktype"
 import { createDataConstraint } from ".."
 import { navigateArray } from "@sdkit/utils"
 
-type Base<T extends Type = Type> = {
-    s: T
-    n: T["infer"]
-}
+import { Type, type } from "arktype"
 
-// type Example = Base & Partial<{ [key: string]: Example }>
-
-function createBase<T extends Type>(props: Base<T>): Base<T> {
-    return props
-}
-
-const test = createBase({
-    s: type("boolean"),
-    n: ""
-})
-
-export type DataConstraintOption<Schema extends Type = Type, X extends Type = Type> = {
+export type DataConstraintOption<DataType extends Type, InnerDataType extends Type> = {
     name: string
     description: string
     required: boolean
 } & (
     | {
           type: "group"
-          options: DataConstraintOptions<X>
+          options: DataConstraintOptions<InnerDataType>
       }
     | {
           type: "value"
-          schema: Schema
-          default?: Schema["infer"]
+          schema: DataType
+          default?: DataType["infer"]
       }
 )
 
-export type DataConstraintOptions<Schema extends Type = Type, X extends Type = Type> = Record<
+export type DataConstraintOptions<DataType extends Type, InnerDataType extends Type = Type> = Record<
     string,
-    DataConstraintOption<Schema, X>
+    DataConstraintOption<DataType, InnerDataType>
 >
 
-//
-//
-
-function createDataConstraintOptionsWorking<Schema extends Type, X extends Type>(
-    props: DataConstraintOptions<Schema, X>
-): DataConstraintOptions<Schema, X> {
+function createDataConstraintOptions<DataType extends Type, InnerDataType extends Type>(
+    props: DataConstraintOptions<DataType, InnerDataType>
+): DataConstraintOptions<DataType, InnerDataType> {
     return props
 }
 
-const test = createDataConstraintOptionsWorking({
+export const test = createDataConstraintOptions({
     test: {
-        name: "Test",
-        description: "The test option.",
+        name: "GOOD: Works as Intended",
+        description: "`default` should error on `false`, as it is correctly inferred as a string.",
+        type: "value",
         required: true,
 
-        type: "value",
         schema: type("string"),
         default: false
     },
-    test2: {
-        name: "Test",
-        description: "The test option.",
+    siblingTest: {
+        name: "BAD: Inherits `DataType` generic from `test`",
+        description: "`schema` should not error, and `default` should not be inferred as a string.",
+        type: "value",
         required: true,
 
-        type: "value",
-        schema: type("number"),
+        schema: type("boolean"),
         default: true
     },
-    values: {
-        name: "Values",
-        description: "The allowed options.",
+    groupTest: {
+        name: "GOOD: Works as Intended",
+        description: "Groups allow nested options.",
+        type: "group",
         required: true,
 
-        type: "group",
-
         options: {
-            a: {
-                name: "A",
-                description: "The allowed options.",
+            nestedTest: {
+                name: "GOOD: Works as Intended",
+                description: '`default` should error on `"test"`, as it is correctly inferred as a boolean.',
+                type: "value",
                 required: true,
 
-                type: "value",
                 schema: type("boolean"),
                 default: "test"
             },
 
-            b: {
-                name: "B",
-                description: "The allowed options.",
+            nestedSiblingTest: {
+                name: "BAD: Inherits `DataType` generic from `nestedTest`",
+                description: "`schema` should not error, and `default` should not be inferred as a boolean.",
+                type: "value",
                 required: true,
 
-                type: "value",
                 schema: type("number"),
                 default: 1
-            }
-        }
-    }
-})
-
-//
-// SIMPLE VARIADIC RECORD
-
-type VariadicRecord<Schemas extends Type, Key extends string = string> = {
-    [key in Key]: InnerVariadicRecord<Schemas>
-}
-
-type InnerVariadicRecord<Schemas extends Type> = {
-    c: Schemas
-    d: Schemas["infer"]
-    nested?: VariadicRecord<Schemas>
-}
-
-function createVariadicRecord<Schema extends Type>(props: VariadicRecord<Schema>): VariadicRecord<Schema> {
-    return props
-}
-
-function constraintOption<Schema extends Type>(props: InnerVariadicRecord<Schema>) {
-    return props
-}
-
-// function createVariadicRecord<Schemas extends Array<Type>>(
-//     cons: VariadicRecord<[...Schemas]>
-// ): {
-//     [K in keyof Schemas]: Schemas[K] extends Type ? Schemas[K] : never
-// }
-// function createVariadicRecord(cons: VariadicRecord<Type[]>): unknown[] {
-//     return cons
-// }
-
-const Test2 = createVariadicRecord({
-    a: constraintOption({
-        c: type("boolean"),
-        // infer d from c, individually
-        d: false
-    }),
-    b: constraintOption({
-        c: type("number"),
-        d: 5,
-        nested: {
-            x: constraintOption({
-                c: type("string"),
-                d: true
-            }),
-            y: constraintOption({
-                c: type("boolean"),
-                d: false,
-                nested: {
-                    z: constraintOption({
-                        c: type("number"),
-                        d: "10"
-                    }),
-                    z2: constraintOption({
-                        c: type("string"),
-                        d: false
-                    })
-                }
-            })
-        }
-    })
-})
-
-//
-//
-type Wrapper<Options extends DataConstraintOptions = DataConstraintOptions> = {
-    wrapper: Options
-}
-
-function createDataConstraintOptions<Options extends DataConstraintOptions = DataConstraintOptions>(
-    props: Wrapper<Options>
-): Wrapper<Options> {
-    return props
-}
-
-const test = createDataConstraintOptions({
-    wrapper: {
-        values: {
-            name: "Values",
-            description: "The allowed options.",
-            required: true,
-
-            type: "group",
-
-            options: {
-                a: {
-                    name: "A",
-                    description: "The allowed options.",
-                    required: true,
-
-                    type: "value",
-                    schema: type("boolean"),
-                    default: false
-                },
-
-                b: {
-                    name: "B",
-                    description: "The allowed options.",
-                    required: true,
-
-                    type: "value",
-                    schema: type("number"),
-                    default: "false"
-                }
             }
         }
     }
